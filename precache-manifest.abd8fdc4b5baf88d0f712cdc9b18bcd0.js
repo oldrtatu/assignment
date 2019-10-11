a788835dc4bd5d4d5b5ced898b05ca76174be2ef
rtatu <rohit.tatu896@gmail.com>
2019-10-11 07:33:47 +0530

    Updates

---
+++ precache-manifest.abd8fdc4b5baf88d0f712cdc9b18bcd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623a63215ffb8f6f51d15d11430440c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d1fff89b78182ba3079",
+    "url": "./static/css/main.76ab24ed.chunk.css"
+  },
+  {
+    "revision": "e1318d3b2073d03f744d",
+    "url": "./static/js/2.bfc8fa16.chunk.js"
+  },
+  {
+    "revision": "5d1fff89b78182ba3079",
+    "url": "./static/js/main.4efa8617.chunk.js"
+  },
+  {
+    "revision": "a5006aece409f13e664e",
+    "url": "./static/js/runtime~main.84bc609c.js"
+  }
+]);
